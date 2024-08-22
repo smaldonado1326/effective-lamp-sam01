1296b55bd5de207a39dadd2ee7c18d01938b0a9b
Maldonado <smaldonado@una.edu>
2024-08-22 13:42:54 -0500

    started express

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('on node server');
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello node from Ex on local dev box')
+})
+
+app.listen(3000)
