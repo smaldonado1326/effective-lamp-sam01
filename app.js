25586b0697f132b82c2c2165d4e8acf3466266e0
smaldonado1326 <smaldonado@una.edu>
2024-08-28 18:15:16 -0500

    update 2

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 console.log('on node server');
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
-
-/*
-app.get('/', function (req, res) {
-  res.send('Hello node from Ex on local dev box')
-})
-*/
 
 app.use(express.static(path.join('./')));
+app.get('/', function (req, res) {
+  //res.send('Hello node from Ex on local dev box')
+});
+
+
+
 
 app.listen(3000, () =>{
 
