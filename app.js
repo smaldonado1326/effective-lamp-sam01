4ef542373ecb65759bcb32a94d043e9eb9cc895a
smaldonado1326 <smaldonado@una.edu>
2024-08-28 18:37:33 -0500

    3

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-console.log('on node server');
+
+
+// console.log('on node server');
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+
+// app.use(express.static(path.join('./')));
+// app.get('/', function (req, res) {
+ 
+// });
+
+
+
+
+// app.listen(3000, () =>{
+
+// });
+
+
+console.log('im on a node server, yo');
+
+
 const express = require('express');
-const path = require('path');
 const app = express();
 
 
 app.use(express.static(path.join('./')));
 app.get('/', function (req, res) {
-  //res.send('Hello node from Ex on local dev box')
+  res.send('Hello Node from Ex on local dev box')
 });
 
-
-
-
-app.listen(3000, () =>{
-
-});
-
+app.listen(3000);
