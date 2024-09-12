7993a1917c758afb3401cc9cf559d2ffc257666a
smaldonado1326 <Smaldonado@una.edu>
2024-09-12 01:02:19 -0500

    9121

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI; 
 
+
+
+
 app.set('view engine', 'ejs')
 app.use(express.static('./public/'))
-
 console.log(uri);
 
-console.log('im on a node server change that and that tanad f, yo');
+console.log('im on a node server...');
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,7 +30,7 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Pinged your deployment.connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
