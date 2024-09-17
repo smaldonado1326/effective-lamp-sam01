ee3ecf83c57514027c1549fdb0952caa845bf52a
smaldonado1326 <Smaldonado@una.edu>
2024-09-16 21:10:37 -0500

    final

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 console.log(process.env.MONGO_URI);
 const express = require('express')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectID } = require('mongodb');
 const uri = process.env.MONGO_URI; 
 
 
@@ -96,35 +96,53 @@ app.get('/insert', async (req, res)=>{
 
 });
 
-app.post('/update/:id', async (req,res)=>{
+app.post('/update/:id', async (req, res) => {
+  console.log("req.params.id: ", req.params.id);
+  try {
+    const collection = client.db("Effective-lamp").collection("Effective-lamp");
+    let result = await collection.findOneAndUpdate(
+      { "_id": new ObjectId(req.params.id) }, // Ensure ObjectId is used correctly
+      { $set: { "post": "NEW POST" } }
+    );
+    console.log(result);
+    res.redirect('/read');
+  } catch (err) {
+    console.error("Error updating data in MongoDB:", err);
+    res.status(500).send("Error updating data in MongoDB");
+  }
+});
 
-  console.log("req.parms.id: ", req.params.id)
 
-  client.connect; 
-  const collection = client.db("Effective-lamp").collection("Effective-lamp");
-  let result = await collection.findOneAndUpdate( 
-  {"_id": new ObjectId(req.params.id)}, { $set: {"post": "NEW POST" } }
-)
-.then(result => {
-  console.log(result); 
-  res.redirect('/read');
-})
+app.get('/read', async (req, res) => {
+  console.log('in /read');
+  try {
+    let result = await client.db("Effective-lamp").collection("Effective-lamp").find({}).toArray();
+    console.log(result);
+    res.render('mongo', {
+      postData: result
+    });
+  } catch (err) {
+    console.error("Error fetching data for /read route:", err);
+    res.status(500).send("Error fetching data for /read route");
+  }
+});
 
 
-app.get('/update' ,async (red, res)=>{
-  console.log('in/ update');
-  res.render('update', {
-    postData: postData
-  })
-  await client.connect();
-  await client.db("Effective-lamp").collection("Effective-lamp").findOneAndUpdate({"post" : "another day"},
-  {$set: {"post": "the next other day"}}
 
-  );
-  res.render('update');
-
-})
-
+app.get('/update', async (req, res) => {
+  console.log('in /update');
+  try {
+    await client.db("Effective-lamp").collection("Effective-lamp").findOneAndUpdate(
+      { "post": "another day" },
+      { $set: { "post": "the next other day" } }
+    );
+    res.render('update', {
+      postData: "Data has been updated"  // Adjust the data passed to the view
+    });
+  } catch (err) {
+    console.error("Error updating data in MongoDB:", err);
+    res.status(500).send("Error updating data in MongoDB");
+  }
 });
 
 app.listen(3000)
